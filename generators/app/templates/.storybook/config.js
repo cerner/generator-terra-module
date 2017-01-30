da08597c7685f5ac129fda27a1514c0f1114ab12
tz043867 <tao.zhang@cerner.com>
2017-01-30 10:07:54 -0600

    Added storybook addons

--- generators/app/templates/.storybook/config.js
+++ generators/app/templates/.storybook/config.js
@@ -1,7 +1,10 @@
-import { configure } from '@kadira/storybook';
+import { configure, setAddon } from '@kadira/storybook';
+import withPropsCombinations from 'react-storybook-addon-props-combinations';
 
 function loadStories() {
   require('../stories');
 }
+
+setAddon(withPropsCombinations)
 
 configure(loadStories, module);
