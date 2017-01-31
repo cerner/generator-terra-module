7cfbc83a798eaee860df27676a41ab0da107994f
tz043867 <tao.zhang@cerner.com>
2017-01-31 14:27:24 -0600

    Move options back to index jsx

--- generators/app/templates/.storybook/config.js
+++ generators/app/templates/.storybook/config.js
@@ -1,23 +1,11 @@
 /* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
 import { configure, setAddon } from '@kadira/storybook';
 import withPropsCombinations from 'react-storybook-addon-props-combinations';
-import { setOptions } from '@kadira/storybook-addon-options';
 
 function loadStories() {
   require('../stories');
 }
 
 setAddon(withPropsCombinations);
-
-setOptions({
-  name: '<%= jsxFileName %>',
-  url: 'https://github.com/cerner/terra-<%= jsxFileName.toLowerCase() %>',
-  goFullScreen: false,
-  showLeftPanel: true,
-  showDownPanel: true,
-  showSearchBox: false,
-  downPanelInRight: false,
-  sortStoriesByKind: false,
-});
 
 configure(loadStories, module);
--- generators/app/templates/stories/index.jsx
+++ generators/app/templates/stories/index.jsx
@@ -3,8 +3,20 @@ import React from 'react';
 import { storiesOf, addDecorator } from '@kadira/storybook';
 import { checkA11y } from 'storybook-addon-a11y';
 import 'storybook-addon-i18n-tools';
+import { setOptions } from '@kadira/storybook-addon-options';
 
 import <%= jsxFileName %> from '../src/<%= jsxFileName %>';
+
+setOptions({
+  name: '<%= jsxFileName %>',
+  url: 'https://github.com/cerner/terra-<%= jsxFileName.split(/(?=[A-Z])/).join('-').toLowerCase() %>',
+  goFullScreen: false,
+  showLeftPanel: true,
+  showDownPanel: true,
+  showSearchBox: false,
+  downPanelInRight: false,
+  sortStoriesByKind: false,
+});
 
 addDecorator(checkA11y);
 
