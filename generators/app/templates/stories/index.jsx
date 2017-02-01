b9aba1a60f18ae1579cbe9e5942a4c656a09bc64
tz043867 <tao.zhang@cerner.com>
2017-02-01 10:01:24 -0600

    Add knobs addons

--- generators/app/templates/stories/index.jsx
+++ generators/app/templates/stories/index.jsx
@@ -1,9 +1,10 @@
 /* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
 import React from 'react';
 import { storiesOf, addDecorator } from '@kadira/storybook';
+import '@kadira/storybook-addon-knobs';
+import { setOptions } from '@kadira/storybook-addon-options';
 import { checkA11y } from 'storybook-addon-a11y';
 import 'storybook-addon-i18n-tools';
-import { setOptions } from '@kadira/storybook-addon-options';
 
 import <%= jsxFileName %> from '../src/<%= jsxFileName %>';
 
