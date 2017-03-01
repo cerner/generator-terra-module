0588023120070ed828037a9ba1443a0a62ad4709
Ryan Manuel <rfmanuel@gmail.com>
2017-02-28 19:45:11 -0600

    Removing unnecessary tests.

--- test/app.js
+++ test/app.js
@@ -13,34 +13,9 @@ describe('generator-terra-module:app', function () {
       .on('end', done);
   });
 
-  it('creates the dot files', function () {
-    assert.file([
-      '.babelrc',
-      '.eslintignore',
-      '.eslintrc',
-      '.gitignore',
-      '.nvmrc',
-      '.stylelintrc',
-      '.travis.yml'
-    ]);
-  });
-
-  it('creates the github template files', function () {
-    assert.file([
-      '.github/ISSUE_TEMPLATE.md',
-      '.github/PULL_REQUEST_TEMPLATE.md'
-    ]);
-  });
-
   it('creates files', function () {
     assert.file([
-      'CONTRIBUTING.md',
-      'CONTRIBUTORS.md',
-      'LICENSE',
-      'NOTICE',
-      'package.json',
-      'README.md',
-      'RELEASE.md'
+      'README.md'
     ]);
   });
 
@@ -54,21 +29,10 @@ describe('generator-terra-module:app', function () {
   it('fills the sass file with some dummy sass', function () {
     assert.fileContent('src/cake.scss', '@import \'./variables\';');
     assert.fileContent('src/cake.scss', '@import \'./mixins\';');
-    assert.fileContent('src/cake.scss', '.waffle-Cake');
   });
 
   it('fills the docs/README file with title cased project data', function () {
     assert.fileContent('docs/README.md', 'Waffle Cake Documentation');
-  });
-
-  it('fills the CONTRIBUTING file with project data', function () {
-    assert.fileContent('CONTRIBUTING.md', 'Browser, and waffle-cake');
-    assert.fileContent('CONTRIBUTING.md', 'https://github.com/cerner/waffle-cake/issues');
-  });
-
-  it('fills the NOTICE file with project data', function () {
-    var currentDate = new Date().getFullYear();
-    assert.fileContent('NOTICE', 'Copyright ' + currentDate);
   });
 
   it('fills the package.json file with project data', function () {
@@ -83,9 +47,5 @@ describe('generator-terra-module:app', function () {
     assert.fileContent('README.md', 'npm install waffle-cake');
     assert.fileContent('README.md', 'https://github.com/cerner/waffle-cake/archive/master.zip');
     assert.fileContent('README.md', 'https://github.com/cerner/waffle-cake.git');
-  });
-
-  it('fills the RELEASE file with project data', function () {
-    assert.fileContent('RELEASE.md', 'https://www.npmjs.com/package/waffle-cake/');
   });
 });
