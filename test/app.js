495f0652a3f0b0be344e4d0243cb9868adc6de23
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2017-07-14 14:19:08 -0500

    Remove night watch config files and update night watch test scripts.

--- test/app.js
+++ test/app.js
@@ -19,8 +19,7 @@ describe('generator-terra-module:app', function () {
       assert.file([
         'packages/terra-waffle-cake/README.md',
         'packages/terra-waffle-cake/CHANGELOG.md',
-        'packages/terra-site/src/examples/waffle-cake/Index.jsx',
-        'packages/terra-waffle-cake/tests/nightwatch.conf.js'
+        'packages/terra-site/src/examples/waffle-cake/Index.jsx'
       ]);
     });
 
@@ -51,6 +50,7 @@ describe('generator-terra-module:app', function () {
     it('fills the package.json file with project data', function () {
       assert.fileContent('packages/terra-waffle-cake/package.json', 'git+https://github.com/cerner/terra-core.git');
       assert.fileContent('packages/terra-waffle-cake/package.json', 'https://github.com/cerner/terra-core/issues');
+      assert.fileContent('packages/terra-waffle-cake/package.json', '../../node_modules/terra-toolkit/lib/scripts');
       assert.fileContent('packages/terra-waffle-cake/package.json', `"props-table": "props-table ./src/WaffleCake.jsx --out-dir ./docs/props-table",`);
     });
 
@@ -95,8 +95,7 @@ describe('generator-terra-module:app', function () {
       assert.file([
         'packages/terra-clinical-monster-cookies/README.md',
         'packages/terra-clinical-monster-cookies/CHANGELOG.md',
-        'packages/terra-clinical-site/src/examples/monster-cookies/Index.jsx',
-        'packages/terra-clinical-monster-cookies/tests/nightwatch.conf.js'
+        'packages/terra-clinical-site/src/examples/monster-cookies/Index.jsx'
       ]);
     });
 
@@ -127,7 +126,6 @@ describe('generator-terra-module:app', function () {
     it('fills the package.json file with project data', function () {
       assert.fileContent('packages/terra-clinical-monster-cookies/package.json', 'git+https://github.com/cerner/terra-clinical.git');
       assert.fileContent('packages/terra-clinical-monster-cookies/package.json', 'https://github.com/cerner/terra-clinical/issues');
-      assert.fileContent('packages/terra-clinical-monster-cookies/package.json', 'WEBPACK_CONFIG_PATH=../../../../terra-clinical-site/webpack.config');
       assert.fileContent('packages/terra-clinical-monster-cookies/package.json', '../../node_modules/terra-toolkit/lib/scripts');
       assert.fileContent('packages/terra-clinical-monster-cookies/package.json', `"props-table": "$(cd ..; npm bin)/props-table ./src/MonsterCookies.jsx --out-dir ./docs/props-table",`);
     });
