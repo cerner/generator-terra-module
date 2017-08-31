755324d28286946da575efc1f8fde07f12f104f7
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2017-08-31 18:01:54 -0500

    Nighwatch uplift.

--- generators/app/templates/projectName-spec.js
+++ generators/app/templates/projectName-spec.js
@@ -1,19 +1,11 @@
-/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+/* eslint-disable no-unused-expressions */
 // eslint-disable-next-line import/no-extraneous-dependencies
-const screenshot = require('terra-toolkit').screenshot;
+const resizeTo = require('terra-toolkit/lib/nightwatch/responsive-helpers').resizeTo;
 
-module.exports = {
-  before: (browser, done) => {
-    browser.resizeWindow(browser.globals.width, browser.globals.height, done);
-  },
-
-  afterEach: (browser, done) => {
-    screenshot(browser, '<%= projectName %>', done);
-  },
-
+module.exports = resizeTo(['tiny', 'small', 'medium', 'large', 'huge', 'enormous'], {
   'Displays a default <%= moduleName %>': (browser) => {
     browser
       .url(`http://localhost:${browser.globals.webpackDevServerPort}/#/tests/<%= moduleName %>-tests/default`)
-      .assert.elementPresent('.<%= cssClassName %>');
+      .expect.element('.<%= cssClassName %>').to.be.present;
   },
-};
+});
