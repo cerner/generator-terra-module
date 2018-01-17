9e0e0906d47d48305428c1d6374357a09e964229
Mike Hemesath <mike.hemesath@gmail.com>
2018-01-17 10:06:00 -0600

    Fix broken tests.
    Add name property back in as content so tests pass in generated code.

--- generators/app/templates/src/projectName.jsx
+++ generators/app/templates/src/projectName.jsx
@@ -24,7 +24,7 @@ const <%= moduleClassName %> = ({ name, ...customProps }) => {
     attributes.className,
   ]);
 
-  return (<div {...attributes} className={<%= moduleClassName %>ClassNames} />);
+  return (<div {...attributes} className={EmbeddedComponentClassNames}>{name}</div>);
 };
 
 <%= moduleClassName %>.propTypes = propTypes;
