bbc21d33d3665596b95b6652a37e152a247b4dbc
Brett Jankord <bjankord@gmail.com>
2019-08-16 11:26:43 -0500

    Update DefaultDocExample.jsx

--- generators/app/templates/src/terra-dev-site/DefaultDocExample.jsx
+++ generators/app/templates/src/terra-dev-site/DefaultDocExample.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-
-// eslint-disable-next-line import/no-extraneous-dependencies, import/no-unresolved, import/extensions
-import <%= moduleClassName %> from '<%= projectName %>/lib/<%= moduleClassName %>';
+import <%= moduleClassName %> from '<%= projectName %>';
 
 export default () => <<%= moduleClassName %> />;
